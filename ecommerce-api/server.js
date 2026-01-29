f48bfac65a19a6ca63981ef1c69c71123ce66790
GusSegura <gustavo.igemer@gmail.com>
2026-01-29 08:28:54 -0600

    cambios en server console log para revosar por que no levanta en render

--- ecommerce-api/server.js
+++ ecommerce-api/server.js
@@ -108,6 +108,7 @@ app.use(errorHandler);
 // });
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('PORT ENV:', process.env.PORT);
   console.log(`Server running on port ${PORT}`);
 });
